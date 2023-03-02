9c1124206aebfe91ff7e1a0f0d878a919895d3f9
Aashika1020 <125243238+Aashika1020@users.noreply.github.com>
2023-03-02 16:35:25 +0400

    EmailValidation
    smtg

--- JAVASCRIPT.js
+++ JAVASCRIPT.js
@@ -18,6 +18,7 @@ let signinButton =  document.getElementById("signinButton");
 let nameField =  document.getElementById("nameField");
 let titlesignup =  document.getElementById("titlesignup");
 
+
 signinButton.onclick = function(){
     nameField.style.maxHeight = "0";
     titlesignup.innerHTML = "Sign In";
@@ -31,4 +32,33 @@ signupButton.onclick = function(){
     signupButton.classList.remove("disable");
     signinButton.classList.add("disable");
 
+}
+
+function validateEmail()
+{
+    var form = document.getElementById("form");
+    var email= document.getElementById("email").value;
+    var text= document.getElementById("text");
+    var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    
+    if(email.match(pattern)){
+        form.classList.add("valid");
+        form.classList.remove("invalid");
+        text.innerHTML = "Your Email Address is Valid";
+        text.style.color = "#006400";
+    }
+    else{
+        form.classList.remove("valid");
+        form.classList.remove("invalid");
+        text.innerHTML = "Please Enter Valid Email Address";
+        text.style.color = "#8b0000";
+    }
+    if(email == ""){
+        form.classList.remove("valid");
+        form.classList.remove("invalid");
+        text.innerHTML = " ";
+        text.style.color = "#00ff00";
+    }
+  
+    
 }
